e573a7dc7f26a5d130808f28ae75b2ac21b2bc37
edurodrigues0 <edusacramento9@gmail.com>
2024-04-01 19:51:45 -0300

    Gerando token JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { CreatedAccountController } from './controllers/create-account.controlle
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate-controller'
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { AuthModule } from './auth/auth.module'
     }),
     AuthModule,
   ],
-  controllers: [CreatedAccountController],
+  controllers: [CreatedAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
